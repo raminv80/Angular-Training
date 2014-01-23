a577c7550059d29aac69ec39c1e17a0c5e6b0853
Ramin Vakilian <ramin@mindvalley.com>
2014-01-23 16:50:19 +0800

    fix

--- 7-challenge-list-management/script.js
+++ 7-challenge-list-management/script.js
@@ -12,33 +12,24 @@ function aController($scope){
 
 	$scope.init = function(){
 		$scope.loadBasket();
-		//Ajax get list of items
 	}
 
 	$scope.loadBasket = function(){
 		$scope.basket = order_list;
-		//Ajax get list of items
 	}
 
-	$scope.list = function(){
-		return $scope.basket;
-		//Ajax get list of items
-	}
-
-	$scope.create = function(){
+	$scope.addItem = function(){
 		$scope.new_fruit.id = ++$scope.id
 		$scope.basket.push($scope.new_fruit)
 		$scope.new_fruit = {name:null, price:null}
-		//Ajax add list of items and reload the data
 	}
 
-	$scope.delete = function(fruit){
+	$scope.removeItem = function(fruit){
 		var items = $scope.basket;
 		for (var i = 0; i < items.length; i++)
     if (items[i].id && items[i].id === fruit.id) { 
         items.splice(i, 1);
         break;
     }
-    //Ajax remove item from list and reload the data
 	}
 }
